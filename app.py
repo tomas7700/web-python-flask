a5c01d05b7da09f9bb48d684efc0394102a25dcc
tomas7700 <tomasalmanzarparra@gmail.com>
2023-06-29 22:27:16 -0500

    add db

--- app.py
+++ app.py
@@ -1,10 +1,20 @@
 from flask import Flask, render_template
+from database import user_info_capture
 
 app = Flask(__name__)
+
+
 
 @app.route('/')
 def hello():
     return render_template('base.html', company_name='IndEvo')
+
+
+@app.route('/user')
+
+def users_info():
+    user_id = user_info_capture()
+    return render_template('user.html',user= user_id , company_name='IndEvo')
 
 # Asignamos la ruta del index
 if __name__ == '__main__':
--- database.py
+++ database.py
@@ -1,5 +1,7 @@
 from sqlalchemy import create_engine, text
-db_string = 'mysql+pymysql://b2dp0z2ds9xocq9vbt3k:pscale_pw_8PAOHHHkLt50RYPMFyrcDZrtqINJlfuoEIYJbQN2Pma@aws.connect.psdb.cloud/indevo?charset=utf8mb4'
+import os
+
+db_string = os.environ.get('KEY_DB')
 engine = create_engine(
     db_string, 
     connect_args={
@@ -18,3 +20,14 @@ with engine.connect() as conn:
           final_result.append(row._asdict())
 
     print(final_result)
+
+def user_info_capture():
+    with engine.connect() as conn:
+        results = conn.execute(text('SELECT * FROM user_log_info'))
+        final_result = []
+        for row in results.all():
+          final_result.append(row._asdict())
+        return final_result
+
+
+
